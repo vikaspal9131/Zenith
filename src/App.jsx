c330c75af7aefb95e1e8d0d6dd830276b3ce9011
vikaspal9131 <vikaspal968562@gmail.com>
2025-09-17 18:19:18 +0530

    init: add base project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
   return (
     <>
-      <h1>Hello world</h1>
+     <h1 className="text-8xl text-emerald-900">hello world we are here </h1>
     </>
-  )
-}
-
-export default App
+  );
+};
